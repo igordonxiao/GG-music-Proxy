33e78975b9edf7c390cc0adfb8d6e96209f03b15
igordonxiao <igordonxiao@gmail.com>
2018-03-29 17:44:07 +0800

    add home page info

--- src/main/kotlin/io/igordonxiao/github/ggmusic/GgmusicApplication.kt
+++ src/main/kotlin/io/igordonxiao/github/ggmusic/GgmusicApplication.kt
@@ -2,6 +2,7 @@ package io.igordonxiao.github.ggmusic
 
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jsoup.Jsoup
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.FilterRegistrationBean
@@ -39,15 +40,39 @@ class GgmusicApplication
 
 @Controller
 class GgmusicController {
-    // 全民UID
-    val kgUid = "639d9d87232a328835"
     var client = OkHttpClient()
 
+    @GetMapping("/songAuthor")
+    @ResponseBody
+    fun songAuthor(@RequestParam uid: String): Map<String, String> {
+        val doc = Jsoup.connect("http://node.kg.qq.com/personal?uid=$uid").get()
+        val authorNameEl = doc.select("body > div.mod_wrap.j_personal_main > div.my_show > div.my_show__con > div.my_show__user > span.my_show__name")
+        val authorImgEl = doc.select("body > div.mod_wrap.j_personal_main > div.my_show > div.my_show__con > div.my_show__photo > img")
+        val levelEl = doc.select("body > div.mod_wrap.j_personal_main > div.my_show > div.my_show__con > div:nth-child(3)")
+        val songCountEl = doc.select("#ugc")
+        return mapOf(
+                "name" to authorNameEl.text(),
+                "img" to authorImgEl.attr("src"),
+                "level" to levelEl.text(),
+                "songCount" to songCountEl.text()
+        )
+    }
+
+    @GetMapping("/songLyric")
+    @ResponseBody
+    fun songLyric(@RequestParam ksongmid: String): String {
+        val url = "http://node.kg.qq.com/cgi/fcgi-bin/fcg_lyric?jsonpCallback=callback_0&outCharset=utf-8&format=json&ksongmid=$ksongmid"
+        val request = Request.Builder()
+                .url(url)
+                .build()
+        val response = client.newCall(request).execute()
+        return response.body()!!.string()
+    }
 
     @GetMapping("/list")
     @ResponseBody
-    fun songList(@RequestParam page : Int = 1, @RequestParam @Nullable size: Int = 10): String {
-        val url = "http://node.kg.qq.com/cgi/fcgi-bin/kg_ugc_get_homepage?type=get_ugc&start=$page&num=$size&share_uid=$kgUid"
+    fun songList(@RequestParam uid: String, @RequestParam page: Int = 1, @RequestParam @Nullable size: Int = 10): String {
+        val url = "http://node.kg.qq.com/cgi/fcgi-bin/kg_ugc_get_homepage?type=get_ugc&start=$page&num=$size&share_uid=$uid"
         val request = Request.Builder()
                 .url(url)
                 .build()
@@ -57,7 +82,7 @@ class GgmusicController {
 
     @GetMapping("/song")
     @ResponseBody
-    fun songInfo(@RequestParam shareId : String): String {
+    fun songInfo(@RequestParam shareId: String): String {
         val url = "http://cgi.kg.qq.com/fcgi-bin/kg_ugc_getdetail?inCharset=GB2312&outCharset=utf-8&v=4&shareid=$shareId"
         val request = Request.Builder()
                 .url(url)
